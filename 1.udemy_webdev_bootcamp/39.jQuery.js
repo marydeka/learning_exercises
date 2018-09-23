7c6dd00b66e9e1409674233357ff15724e8a9a43
Mary Deka <mary.m.deka@gmail.com>
2018-09-23 13:51:29 -0500

    first jQuery exercise

---
+++ 1.udemy_webdev_bootcamp/39.jQuery.js
@@ -0,0 +1,10 @@
+//if(jQuery){
+	//alert("jQuery Loaded!");
+// } else {
+	//alert("No jQuery");
+//}
+
+$("div").css("background", "purple");
+$("div.highlight").css("width", "200px");
+$("#third").css("border", "2px solid orange");
+$("div:first-of-type").css("color", "pink");
