d982d4277cd4bff89e1c5362b88bfed83e522bc7
Mary Deka <mary.m.deka@gmail.com>
2018-10-30 02:32:58 +0000

    adding IntroToNode exercises

---
+++ 1.udemy_webdev_bootcamp/43.IntroToNode/grader.js
@@ -0,0 +1,18 @@
+function average(grades){
+    var sum = 0;
+    for(var i = 0; i < grades.length; i++){
+      sum += grades[i];
+      var avg = sum/grades.length;
+    }
+    
+    return Math.round(avg);
+}
+
+    var scores = [90, 98, 89, 100, 100, 86, 94];
+    console.log(average(scores));
+    var scores2 = [40, 65, 77, 82, 80, 54, 73, 63, 95, 49];
+    console.log(average(scores2));
+
+//can use scores.forEach(function(score){
+    //total += score;
+//}
